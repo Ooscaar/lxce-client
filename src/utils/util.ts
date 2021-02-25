33770fe932917c9907dd30e2b18073dab628d1c2
Oscar Perez <ooscar.perez@protonmail.com>
2021-02-25 17:53:19 +0100

    utils: add general functions
    Added:
     - checkAlias: general function (string and array) for checking alias

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -3,6 +3,8 @@ import * as util from "util"
 import * as fs from "fs"
 import * as child from "child_process"
 import { error } from "console"
+import path from "path"
+import { CONTAINER_CONFIG_DIR, SSH_DIR } from "../constants"
 
 // Constants
 const exec = util.promisify(child.exec);
@@ -66,25 +68,90 @@ export function readJSON(filePath: string): any {
 }
 
 
-/**
- * Exec commands and print outputs
- *
- * @param command
- * @param dry
- * @param debug
- */
-export async function cmd(command: string, dry = false, debug = false) {
-    try {
-        if (dry) console.log("DRY: " + command);
-        else {
-            const { stdout, stderr } = await exec(command);
-            if (debug) console.log('Exec: ' + command);
-            if (stdout) console.log(stdout);
-            if (stderr) console.log(stderr);
-        }
-    } catch (err) {
-        //console.error(error(err));
-        // TODO: implement with error
-        console.log(error)
+
+// Check all configurations files
+// TODO: depending of folders structure
+export function existAlias(argAlias: string | Array<string>): boolean {
+
+    // To make the function more general
+    // https://stackoverflow.com/questions/35749833/typescript-function-taking-one-or-array-of-objects
+    let alias: Array<string> = []    
+    alias = alias.concat(argAlias)
+    console.log("[**] Debug: alias,", alias)
+    let existingAlias: Array<string> = []
+
+    const dirCont = fs.readdirSync(CONTAINER_CONFIG_DIR)
+
+    for (const file of dirCont) {
+        // It return relative paths
+        let filePath = path.join(CONTAINER_CONFIG_DIR, file)
+        let configFile = readJSON(filePath)
+        existingAlias.push(configFile.alias)
     }
+    console.log("[**] Debug: existing alias", existingAlias)
+
+    for (const cAlias of alias) {
+        if (existingAlias.includes(cAlias)) {
+            console.log("[**] Debug: compare", cAlias)
+            return true 
+        }
+    }
+
+    return false
+
+}
+
+
+export function existName(name: string): boolean {
+    const locations = fs.readdirSync(CONTAINER_CONFIG_DIR)
+    return locations.includes(name)
+}
+
+// We don't check the existence of name
+// as it should be checked before
+export function getNamefromAlias(alias: string): string {
+    for (const loc of fs.readdirSync(CONTAINER_CONFIG_DIR)) {
+        let config = readJSON(
+            path.join(CONTAINER_CONFIG_DIR, loc)
+        )
+        if (config.alias == alias) return loc
+    }
+    // For typescript message
+    return ""
+
+}
+
+// Get container name from alias or name
+// and exits the process in case of 
+// not founding neither of them
+export function getName(argsAlias: string, argsName: string): string {
+    let name = ""
+    if (argsAlias) {
+        if (! existAlias(argsAlias)) {
+            console.log(`[*] Alias ${argsAlias} does not exist`)
+            process.exit(1)
+        }
+        name = getNamefromAlias(argsAlias)
+    } else {
+        if (! existName(argsName)) {
+            console.log("[*] Name does not exist")
+            process.exit(1)
+        }
+        name = argsName
+    }
+    return name
+}
+
+// Check if has access to configuations directories
+// - Write
+// TODO: add shared directories also?
+export function checkAcces(): boolean {
+    try {
+        fs.accessSync(CONTAINER_CONFIG_DIR, fs.constants.W_OK)
+        fs.accessSync(SSH_DIR, fs.constants.W_OK)
+        return true
+    } catch(err) {
+        return false
+    }
+
 }
