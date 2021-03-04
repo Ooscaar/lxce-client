ff691090d0bedd3f1d995cff5e3c7d16b13718ac
Oscar Perez <ooscar.perez@protonmail.com>
2021-03-04 18:29:30 +0100

    cmds/proxy: first version of proxy

--- src/cmds/proxy.ts
+++ src/cmds/proxy.ts
@@ -1,10 +1,146 @@
-import * as fs from "fs"
+import { execSync } from "child_process"
+import path from "path"
+import { CONF_FILE, CONTAINER_CONFIG_DIR } from "../constants"
+import { checkAcces, checkDomain, checkInitialized, getName, readContainerConfig, readJSON, readLxceConfig } from "../utils/util"
+import { Proxy } from "../interfaces/interfaces"
 import yargs from "yargs"
+import * as fs from "fs"
+import { launchProxies } from "./launch"
 
 
-// Install function
-export async function cmdProxy(args: yargs.Arguments) {
-    console.log("Not implemented")
-    process.exit(1)
+// interface Proxy {
+//     name: string,
+//     type: string,
+//     listen: string
+//     port: string
+// }
+
+
+// TODO: move function to utils
+export function lxcProxy(name: string, hostPort: number, cHostname: string, proxy: Proxy) {
+    let proxyDevice = `proxy-${proxy.name}`
+    let command = `lxc config device add ${name} ${proxyDevice} proxy\
+    listen=${proxy.type}:${proxy.listen}:${hostPort}\
+    connect=${proxy.type}:${cHostname}:${proxy.port}`
+
+    try {
+        execSync(command)
+        console.log("[**] added proxy")
+    } catch (err) {
+        console.error(err.message)
+        process.exit(1)
+    }
+
+}
+
+
+function removeProxy(name: string, proxyName: string) {
+    try {
+        let command = `lxc config device remove ${name} ${proxyName}`
+        execSync(command)
+    } catch (err) {
+        console.log(err.message)
+    }
+
+}
+
+// Using:
+// lxc config device list ...
+// as at the moment of calling the function
+// the existing proxies have changed in the 
+// configuration file of lxce
+function removeProxies(name: string) {
+    try {
+        // Filtering data devices
+        let proxiesNames = execSync(`lxc config device list ${name}`)
+            .toString()
+            .split("\n")
+            .filter(elem => elem.startsWith("proxy-"))
+        for (let proxyName of proxiesNames) {
+            console.log(`[**] Removing proxy: ${proxyName}`)
+            removeProxy(name, proxyName)
+        }
+
+    } catch (err) {
+        console.error(err.message)
+    }
+
+}
+
+function checkProxy(domain: string) {
+
+    if (!checkInitialized()) {
+        yargs.showHelp()
+        console.log("[*] run lxce init before")
+        process.exit(1)
+    }
+
+    if (domain) {
+        if (!checkDomain(domain)) {
+            yargs.showHelp()
+            console.log("[*] Domain does not exist")
+            process.exit(1)
+        }
+    }
+
+}
+
+export function cmdProxy(args: any) {
+
+    if (!args.name && !args.global && !args.domain) {
+        yargs.showHelp()
+        console.log("[*] Please select at least one option")
+        process.exit(1)
+    }
+
+    checkProxy(args.domain)
+
+    const lxceConfig = readLxceConfig(CONF_FILE)
+    if (args.global) {
+        console.log("[*] Running global")
+        let domains = fs.readdirSync(CONTAINER_CONFIG_DIR)
+        for (let domain of domains) {
+            for (let containerName of fs.readdirSync(path.join(CONTAINER_CONFIG_DIR, domain))) {
+                let containerPath = path.join(CONTAINER_CONFIG_DIR, domain, containerName)
+                let containerConfig = readContainerConfig(containerPath)
+                removeProxies(containerName)
+                launchProxies(containerName, lxceConfig.hypervisor.SSH_hostname, containerConfig)
+            }
+
+        }
+        process.exit(0)
+    }
+
+    if (args.domain && !args.name) {
+        console.log("[*] Running within domain")
+
+        for (let containerName of fs.readdirSync(path.join(CONTAINER_CONFIG_DIR, args.domain))) {
+            let containerPath = path.join(CONTAINER_CONFIG_DIR, args.domain, containerName)
+            let containerConfig = readContainerConfig(containerPath)
+            removeProxies(containerName)
+            launchProxies(containerName, lxceConfig.hypervisor.SSH_hostname, containerConfig)
+        }
+        process.exit(0)
+
+
+    } else {
+        if (!args.domain) {
+            yargs.showHelp()
+            console.log("Please select the domain")
+            process.exit(1)
+        }
+        if (!checkDomain(args.domain)) {
+            console.log("[*] Domain does not exist")
+            process.exit(1)
+        }
+        console.log("[*] Running only one container")
+        let containerName = getName(args.name, args.domain)
+        let containerPath = path.join(CONTAINER_CONFIG_DIR, args.domain, containerName)
+        let containerConfig = readContainerConfig(containerPath)
+        removeProxies(containerName)
+        launchProxies(containerName, lxceConfig.hypervisor.SSH_hostname, containerConfig)
+        process.exit(0)
+    }
+
 
 }
