6a980836cf434697bf5efa3ddf6b65c9aa13f82e
Oscar Perez <ooscar.perez@protonmail.com>
2021-03-04 18:14:23 +0100

    cmds/install: clean structure and use interfaces

--- src/cmds/install.ts
+++ src/cmds/install.ts
@@ -4,7 +4,8 @@ import yargs from "yargs"
 
 //import * as CONST from "../constants"
 import { BASE_DIR, CONTAINER_CONFIG_DEFAULT, CONF_FILE, CONF_FILE_DATA, DEFAULT_CONTAINER_CONF_FILE, SEED_LENGHT, SEED_ENCODING } from "../constants"
-import { writeJSON, generateSeed } from "../utils/util"
+import { ContainerConfig, LxceConfig } from "../interfaces/interfaces"
+import { writeJSON, generateSeed, writeContainerConfig, writeLxceConfig } from "../utils/util"
 
 
 
@@ -12,26 +13,35 @@ import { writeJSON, generateSeed } from "../utils/util"
 export function cmdInstall(args: yargs.Arguments) {
     try {
         if (fs.existsSync(BASE_DIR)) {
-            console.log("The base directory exist at: " + BASE_DIR)
-            console.log("If you want to use install, you should destroy everything that is available.")
-            console.log("Use lxce destroy before install.")
+            console.log("[*] The base directory exist at: " + BASE_DIR)
+            console.log("[*] If you want to use install, you should destroy everything that is available.")
+            console.log("[*] Use lxce destroy before install.")
             process.exit(1)
         }
 
         fs.mkdirSync(BASE_DIR);
 
-        let data = CONF_FILE_DATA;
+        let data: LxceConfig = CONF_FILE_DATA;
         data.seed = generateSeed(SEED_LENGHT, SEED_ENCODING)
-        writeJSON(CONF_FILE, data)
+        //writeJSON(CONF_FILE, data)
+        writeLxceConfig(
+            CONF_FILE,
+            data
+        )
         console.log("Write: ", data)
 
-        writeJSON(
+        // writeJSON(
+        //     DEFAULT_CONTAINER_CONF_FILE,
+        //     CONTAINER_CONFIG_DEFAULT
+        // )
+
+        writeContainerConfig(
             DEFAULT_CONTAINER_CONF_FILE,
             CONTAINER_CONFIG_DEFAULT
         )
         console.log("Write: ", CONTAINER_CONFIG_DEFAULT)
 
-    } catch(err) {
+    } catch (err) {
         // TODO: manage the errors, as we will have:
         // "EACCES": no permisions to execute the syscall
         // "ENOENT": in case of an inexistent file
@@ -42,6 +52,7 @@ export function cmdInstall(args: yargs.Arguments) {
         }
         if (err.code = "ENOENT") {
             console.error("File does not exist")
+            console.error(err)
             process.exit(1)
         }
         console.error(err)
@@ -49,10 +60,10 @@ export function cmdInstall(args: yargs.Arguments) {
     }
 
     // This way or before the catch ?
-    console.log("install successfully");
-    console.log("Complete config file: %s and optinally you can modify the default container configuration", CONF_FILE);
-    console.log("at ('TODO') and use :");
-    console.log("'lxce init' to deploy it.");
+    console.log("[*] install successfully");
+    console.log("[*] Complete config file: %s and optinally you can modify the default container configuration", CONF_FILE);
+    console.log("[*] at ('TODO') and use :");
+    console.log("[*] 'lxce init' to deploy it.");
     process.exit(0)
 
 }
