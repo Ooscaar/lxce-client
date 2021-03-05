fc442ddb4ce6057a4844808d0f55e07a4bc0aa1f
Oscar Perez <ooscar.perez@protonmail.com>
2021-03-05 12:38:19 +0100

    cmds/list: show port numbers

--- src/cmds/list.ts
+++ src/cmds/list.ts
@@ -4,6 +4,7 @@ import { execSync } from "child_process";
 import { readContainerConfig } from "../utils/util";
 import path from "path";
 import * as fs from "fs";
+import { getPortNumber, launchProxies } from "./launch";
 
 
 // Temporal client 
@@ -14,20 +15,35 @@ const URL = "unix://var/snap/lxd/common/lxd/unix.socket"
 function getContainerJSON(name: string, domain: string): any {
     let data = execSync(`curl --unix-socket /var/snap/lxd/common/lxd/unix.socket s/1.0/containers/${name}/state 2>/dev/null`).toString()
     let json = JSON.parse(data,).metadata
+    const containerConfig = readContainerConfig(path.join(CONTAINER_CONFIG_DIR, domain, name))
+
+    // FIXME: Temporal
+    let id_container = containerConfig.id_container
+    let id_domain = containerConfig.id_domain
+    let ports = containerConfig.proxies.map((proxy, index) => {
+        let port = getPortNumber(
+            id_container,
+            id_domain,
+            index
+        )
+        return port
+    })
+
     // Return object according to table
     return {
         name: name,
-        alias: readContainerConfig(path.join(CONTAINER_CONFIG_DIR, domain, name)).alias,
+        alias: containerConfig.alias,
         domain: domain,
         state: json.status ?? "",
-        ipv4: json.network ? json.network.eth0.addresses[0].address : ""
+        ipv4: json.network ? json.network.eth0.addresses[0].address : "",
+        ports: ports.toString()
     }
 }
 
 export function cmdList(args: any) {
 
     // Get info from all containers
-    let data: Array<any> = [["NAME", "ALIAS", "DOMAIN", "STATE", "Ipv4"]]
+    let data: Array<any> = [["NAME", "ALIAS", "DOMAIN", "STATE", "Ipv4", "PORTS"]]
     let domains = fs.readdirSync(CONTAINER_CONFIG_DIR)
     for (let domain of domains) {
         for (let containerName of fs.readdirSync(path.join(CONTAINER_CONFIG_DIR, domain))) {
