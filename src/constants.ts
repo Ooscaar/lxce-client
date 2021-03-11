8872c264f1a641c87314b3361eb29b05620f5fbf
Oscar Perez <ooscar.perez@protonmail.com>
2021-03-11 13:44:50 +0100

    project: add user property to ContainerConfig interface

--- src/constants.ts
+++ src/constants.ts
@@ -26,50 +26,51 @@ export const CONTAINER_CONFIG_DIR = BASE_DIR + 'container.conf.d/';
 export const DEFAULT_CONTAINER_CONF_FILE = BASE_DIR + 'container_default.conf';
 export const CONF_FILE = BASE_DIR + 'lxce.conf';
 export const CONF_FILE_DATA: LxceConfig = {
-  "hypervisor":
-  {
-    "SSH_hostname": "",
-    "SSH_suffix": "",
-  },
-  "seed": "",
-  "domains":
-    [
-      "default",
-    ],
-  "locations":
-    [
-      "/datasdd",
-      "/datahdd"
-    ],
+    "hypervisor":
+    {
+        "SSH_hostname": "",
+        "SSH_suffix": "",
+    },
+    "seed": "",
+    "domains":
+        [
+            "default",
+        ],
+    "locations":
+        [
+            "/datasdd",
+            "/datahdd"
+        ],
 };
 
 export const LXCE_DIR = "lxce/";
 export const CONTAINER_CONFIG_DEFAULT: ContainerConfig = {   // save in DEFAULT_CONTAINER_CONF_FILE
-  name: "",
-  alias: "",
-  id_domain: 0,
-  id_container: 0,
-  domain: 'default',                        // Domain name for the container
-  base: 'ubuntu:20.04',                     // Base name in lxc launch
-  userData: CONF_FILE_DATA.locations[0],    // /datahdd/lxce
-  proxies: [
-    {
-      name: 'ssh',                          // Name of the proxy rule
-      type: 'tcp',                          // tcp / upd
-      listen: '0.0.0.0',                    // IP to listen in the host, typicaly (0.0.0.0 / 127.0.0.1)
-      port: 22,                             // Host port DD-CC-P (10000) goes to container_name.lxd:22 -> DD is domain number (e.g. 10), CC is container number (e.g. 00) and P is port (e.g. 0)
+    name: "",
+    alias: "",
+    user: "",
+    id_domain: 0,
+    id_container: 0,
+    domain: 'default',                        // Domain name for the container
+    base: 'ubuntu:20.04',                     // Base name in lxc launch
+    userData: CONF_FILE_DATA.locations[0],    // /datahdd/lxce
+    proxies: [
+        {
+            name: 'ssh',                          // Name of the proxy rule
+            type: 'tcp',                          // tcp / upd
+            listen: '0.0.0.0',                    // IP to listen in the host, typicaly (0.0.0.0 / 127.0.0.1)
+            port: 22,                             // Host port DD-CC-P (10000) goes to container_name.lxd:22 -> DD is domain number (e.g. 10), CC is container number (e.g. 00) and P is port (e.g. 0)
+        },
+        {
+            name: 'test',                         // Name of the proxy rule
+            type: 'tcp',                          // tcp / upd
+            listen: '0.0.0.0',                    // IP to listen in the host, typicaly (0.0.0.0 / 127.0.0.1)
+            port: 3000,                           // Host port DD-CC-P (10001) goes to container_name.lxd:3000
+        }
+    ],
+    nginx: {
+        novnc: 7000,
+        www: 80,
     },
-    {
-      name: 'test',                         // Name of the proxy rule
-      type: 'tcp',                          // tcp / upd
-      listen: '0.0.0.0',                    // IP to listen in the host, typicaly (0.0.0.0 / 127.0.0.1)
-      port: 3000,                           // Host port DD-CC-P (10001) goes to container_name.lxd:3000
-    }
-  ],
-  nginx: {
-    novnc: 7000,
-    www: 80,
-  },
 
 };
 
@@ -80,35 +81,35 @@ export const CONTAINER_CONFIG_DEFAULT: ContainerConfig = {   // save in DEFAULT_
 import { Config as NamesConfig, adjectives, colors, animals } from 'unique-names-generator';
 
 export const NAMES_CONFIG: NamesConfig = {
-  dictionaries: [adjectives, colors],
-  separator: '-',
-  length: 2,
+    dictionaries: [adjectives, colors],
+    separator: '-',
+    length: 2,
 };
 
 
 // ---------------------------
-// Table configurations 
+// Table configurations
 // ---------------------------
 export const tableConfig = {
-  border: {
-    topBody: `-`,
-    topJoin: `+`,
-    topLeft: `+`,
-    topRight: `+`,
+    border: {
+        topBody: `-`,
+        topJoin: `+`,
+        topLeft: `+`,
+        topRight: `+`,
 
-    bottomBody: `-`,
-    bottomJoin: `+`,
-    bottomLeft: `+`,
-    bottomRight: `+`,
+        bottomBody: `-`,
+        bottomJoin: `+`,
+        bottomLeft: `+`,
+        bottomRight: `+`,
 
-    bodyLeft: `|`,
-    bodyRight: `|`,
-    bodyJoin: `|`,
+        bodyLeft: `|`,
+        bodyRight: `|`,
+        bodyJoin: `|`,
 
-    joinBody: `-`,
-    joinLeft: `+`,
-    joinRight: `+`,
-    joinJoin: `+`
-  },
+        joinBody: `-`,
+        joinLeft: `+`,
+        joinRight: `+`,
+        joinJoin: `+`
+    },
 }
 
--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -1,37 +1,38 @@
 export interface LxceConfig {
-    hypervisor: Hypervisor;
-    seed: string;
-    domains: string[];
-    locations: string[];
+    hypervisor: Hypervisor,
+    seed: string,
+    domains: string[],
+    locations: string[],
 }
 
 export interface Hypervisor {
-    SSH_hostname: string;
-    SSH_suffix: string;
+    SSH_hostname: string,
+    SSH_suffix: string
 }
 
 export interface ContainerConfig {
-    name: string;
-    alias: string;
+    name: string,
+    alias: string,
+    user: string,
     id_domain: number,
     id_container: number,
-    domain: string;
-    base: string;
-    userData: string;
-    proxies: Proxy[];
-    nginx: Nginx;
+    domain: string
+    base: string,
+    userData: string,
+    proxies: Proxy[],
+    nginx: Nginx
 }
 
 export interface Nginx {
-    novnc: number;
-    www: number;
+    novnc: number,
+    www: number
 }
 
 export interface Proxy {
-    name: string;
-    type: string;
-    listen: string;
-    port: number;
+    name: string,
+    type: string,
+    listen: string,
+    port: number,
 }
 
 export interface SSH {
