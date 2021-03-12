474854c557267ee18f39b68f581d714591963d3a
Oscar Perez <ooscar.perez@protonmail.com>
2021-03-12 13:59:49 +0100

    cmds/uninstall: first version
    For the moment:
    - Remove existing lxce containers
    - Remove /etc/lxce directory
    - Remove all locations/etc/ defined on lxce.conf

--- src/cmds/uninstall.ts
+++ src/cmds/uninstall.ts
@@ -1,12 +1,73 @@
 
+import { execSync } from "child_process"
+import path from "path"
 import yargs from "yargs"
+import { BASE_DIR, CONF_FILE, DEFAULT_CONTAINER_CONF_FILE, LXCE_DIR } from "../constants"
+import {
+    askQuestion,
+    checkAccess,
+    checkDomain,
+    checkInitialized,
+    getContainersAll,
+    lxcDelete,
+    readContainerConfig,
+    readLxceConfig
+} from "../utils/util"
 
+function checkUninstall() {
+    // Check Init
+    if (!checkInitialized()) {
+        yargs.showHelp()
+        console.log("[*] run lxce init before")
+        process.exit(1)
+    }
+    console.log("[*] Init: ok!")
+
+
+    // Check write permissions
+    if (!checkAccess()) {
+        console.log("Does not have access to the directory")
+        console.log("Please run the command with sudo")
+        process.exit(1)
+    }
+    console.log("[*] Permission checked")
+
+}
+
+// Delete:
+// - containers
+// - configurations
+// - read only folders
+function uninstall() {
+    // Remove existing/running containers
+    console.log("[*] Deleting and stoping current containers")
+    for (let containerName of getContainersAll()) {
+        lxcDelete(containerName)
+    }
+
+    // TODO: commit the destroy??
+    // Now remove all folders
+    // Get folder locations before removing
+    const locationsAbsolute = readLxceConfig(CONF_FILE).locations
+        .map(loc => path.join(loc, LXCE_DIR))
+    for (let location of locationsAbsolute) {
+        execSync(`rm -rf ${location}`)
+    }
+
+}
 
 // Install function
-function cmdUninstall(args: yargs.Arguments) {
-    console.log("Not implemented")
-    process.exit(1)
+async function cmdUninstall(args: any) {
+    checkUninstall()
 
+    if (!args.yes) {
+        const answer = await askQuestion(`Do you want to uninstall the lxce command and all it's configurations?`)
+        if (!answer) {
+            process.exit(1)
+        }
+    }
+    uninstall()
+    process.exit(0)
 }
 
 
@@ -15,6 +76,16 @@ function cmdUninstall(args: yargs.Arguments) {
 // ---------------------
 export const command = "uninstall"
 
-export const describe = "NOT IMPLEMENTED"
+export const describe = "Remove all configurations from the lxce command"
 
 export const handler = cmdUninstall
+
+
+export const builder = {
+    "yes": {
+        alias: "y",
+        demand: false,
+        type: "boolean",
+        nargs: 0,
+    }
+}
