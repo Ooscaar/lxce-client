a9a3fde07cb1b14e1995959ff9a1a12c6db10e91
Oscar Perez <ooscar.perez@protonmail.com>
2021-03-12 13:58:59 +0100

    project: change clean script

--- bin/clean.sh
+++ bin/clean.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 if [ "$EUID" -ne 0 ]
-    then echo "Please run as root"
+    then echo "[*] Please run as root"
     exit 1
 fi
 rm -rf /home/oscar/m/tfg-lxce/etc/lxce/container.conf.d/*
 rm -rf /home/oscar/m/tfg-lxce/etc/lxce/ssh/*
 rm -rf /home/oscar/m/tfg-lxce/datasdd/lxce/*
+rm -rf /etc/lxce/
+rm -rf /datasdd/lxce/*
 echo "Succes!!"
