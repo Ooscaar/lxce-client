0e852b1d257caf4202b627a092e39f373ef71672
Oscar Perez <ooscar.perez@protonmail.com>
2021-03-22 19:28:06 +0100

    completion: fix zsh completion

--- completions/completion.zsh
+++ completions/completion.zsh
@@ -1,19 +1,19 @@
-###-lxce-completions-###
+###-begin-lxce-completions-###
 #
-# lxce completion for zsh (default completion) 
+# yargs command completion script
 #
-# Temporal:
-# $: cat completions/completion.zsh >> ~/.zshrc
-# $: source completions/completion.zsh
+# Installation: /usr/sbin/lxce completion >> ~/.zshrc
+#    or /usr/sbin/lxce completion >> ~/.zsh_profile on OSX.
 #
-_lxce()
+_lxce_yargs_completions()
 {
   local reply
   local si=$IFS
-  IFS=$'\n' reply=($(COMP_CWORD="$((CURRENT-1))" COMP_LINE="$BUFFER" COMP_POINT="$CURSOR" /usr/sbin/lxce --get-yargs-completions "\${words[@]}"))
+  IFS=$'
+' reply=($(COMP_CWORD="$((CURRENT-1))" COMP_LINE="$BUFFER" COMP_POINT="$CURSOR" /usr/sbin/lxce --get-yargs-completions "${words[@]}"))
   IFS=$si
   _describe 'values' reply
 }
-compdef _lxce() lxce
+compdef _lxce_yargs_completions lxce
 ###-end-lxce-completions-###
 
